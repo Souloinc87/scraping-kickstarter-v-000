0387a61f6d7c29ef1ffae823dd4491b4c0fa34bc
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-08 16:27:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,7 @@ kickstarter = Nokogiri::HTML(html)
 def create_project_hash
   # write your code here
   #projects: kickstarter.css("li.project.grid_4")
+  #title: project.css("h2.bbcard_name strong a").text 
 end
 
 binding.pry
