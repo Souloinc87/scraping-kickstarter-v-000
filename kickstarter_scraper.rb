14da8db99d9395bbad7348dc08d1f9f0adefe629
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-08 16:52:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,6 @@ require 'nokogiri'
 require 'pry'
 
 html = File.read('fixtures/kickstarter.html')
-
 kickstarter = Nokogiri::HTML(html)
 
 #projects: kickstarter.css("li.project.grid_4")
@@ -15,7 +14,10 @@ kickstarter = Nokogiri::HTML(html)
 
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 
+  projects = {}
 end
 
-binding.pry
+#binding.pry
